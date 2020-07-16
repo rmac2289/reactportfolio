89805f5d449d42f254ee050f7bb94412c4c791aa
Ross MacDonald <rmac2289@gmail.com>
2020-07-16 16:22:48 -0700

    styling updates

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './Nav';
 import Footer from './Footer';
 import Burger from './Burger';
+import Projects from './Projects';
 
 
 function HomePage(){
@@ -13,10 +14,13 @@ function HomePage(){
     <Nav />
     <main>
         <header role="banner">
+            <div className="header-box">
             <h1 className="header">ROSS MACDONALD</h1>
             <h2 className="header">full stack web developer in the Portland metro area</h2>
+            </div>
         </header>  
     </main>
+    <Projects/>
     <Footer />
     </div>
     </div>
--- src/Projects.js
+++ src/Projects.js
@@ -20,7 +20,6 @@ export default function Projects(){
                     
                 </main>
             </div>
-            <Footer />
         </div>
     )
 }
